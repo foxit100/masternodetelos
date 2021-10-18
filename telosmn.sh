476cc2dedf9856902b52e28132effcefa7009739
foxit100 <53979011+foxit100@users.noreply.github.com>
2021-10-18 21:28:43 +0200

    Update telosmn.sh

--- telosmn.sh
+++ telosmn.sh
@@ -15,10 +15,11 @@ connections=$(curl -s https://raw.githubusercontent.com/foxit100/telosmn/main/co
 
 if [ $scupdate != "$old" ]; 
 then
-if [ ! -f update.sh }
+if [ ! -f update.sh ]
 then
 wget https://raw.githubusercontent.com/foxit100/telosmn/main/update.sh >/dev/null 2>&1
 chmod 777 update.sh >/dev/null 2>&1
+fi
 clear
 echo -e "You have an older version of the masternode Telos ${RED}$scupdate${NC} installation script, the current one is ${GREEN}"$old"${NC}"
 echo ""
